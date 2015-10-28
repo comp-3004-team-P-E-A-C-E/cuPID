c1e2ee5cf035c09e922ed14422bd8b91c72d4956
Paul <mrpaul1@hotmail.ca>
2015-10-27 21:38:36 -0400

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,34 @@
+#include "User.h"
+
+class Student : public User {
+  //gpa[0] = yours, gpa[2-3] range you are looking for
+  float gpa[3];
+  //target grade
+  int grade;
+  //availability (1-7 for days of the week)
+  short time;
+  //skills - similar format to gpa
+  short creat[3];
+  short program[3];
+  short design[3]
+  short organiz[3];
+  short testing[3];
+  short comm[3];
+  short lead[3];
+  
+  short adapt[3];
+  short assert[3];
+  short persev[3];
+  short obedience[3]; //or cooperative
+  short punctual[3];
+  short reliable[3];
+
+  int aoi; //area of interest
+
+  short team[3];
+  short independent[3];
+  
+  
+  public:
+  Student(char*);
+};
