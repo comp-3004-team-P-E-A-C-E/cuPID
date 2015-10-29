4a33ae56061b2ff49e4db725c80fb9b75fbecabe
Paul <mrpaul1@hotmail.ca>
2015-10-29 14:05:07 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,16 @@
 #include "User.h"
 #include "Admin.h"
 #include "Student.h"
+#include "Project.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
   char* n = "name";
   Admin* u = new Admin(n);
+  Student* s = new Student(n);
+  Project* p = new Project(n, u);
+  p->addStudent(s);
   n = u->name;
-  std::cout << (n) << std::endl;
+  std::cout << p->containsStu(s) << std::endl;
   return 0;
 }
