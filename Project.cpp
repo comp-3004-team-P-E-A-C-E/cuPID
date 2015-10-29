e96beae8a965aea329ee6ce75060ce9946dd5ac0
Paul <mrpaul1@hotmail.ca>
2015-10-29 14:03:30 -0400

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -1,13 +1,33 @@
 #include "Project.h"
 #include "Student.h"
 #include "Admin.h"
+#include <algorithm>
 
-Project::Project(char* n) {
+Project::Project(char* n, Admin* a) {
   name = n;
+  owner = a;
 }
 
 bool Project::addStudent(Student* s) {
-  //something like addPirate from the 2404 stuff, I don't remember how to do it
+  students.push_back(s);
+}
+
+bool Project::removeStudent(Student* s) {
+  std::vector<Student*>::iterator it;
+  for(it = students.begin(); it != students.end(); it++)
+  	if(*it == s) students.erase(it);
+}
+
+bool Project::containsStu(Student* s) {
+  if(std::find(students.begin(), students.end(), s) != students.end())
+    return true;
+  return false;
+}
+
+Student* Project::getStu(int i) {
+  if(i < students.size()-1) 
+    return students[i];
+  return NULL;
 }
 
 void Project::setTeamSize(int i) {
