79efecd40becd8f748ba87e8aea9baf4fbdd4a42
Paul <mrpaul1@hotmail.ca>
2015-10-29 14:04:20 -0400

    Update Project.h

--- Project.h
+++ Project.h
@@ -1,14 +1,19 @@
+#include <vector>
+
 class Student;
 class Admin;
 
 class Project {
   int teamSize;
   char* name;
-  Student* Students;
+  std::vector<Student*> students;
   Admin* owner;
   
   public:
-  Project(char*);
+  Project(char*, Admin*);
   bool addStudent(Student*);
+  bool removeStudent(Student*);
+  bool containsStu(Student*);
+  Student* getStu(int);
   void setTeamSize(int);
 };
