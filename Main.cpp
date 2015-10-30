6db71d8a6fd2da1725104a2c910f779de41d8203
Paul <mrpaul1@hotmail.ca>
2015-10-30 16:57:09 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,27 @@
 #include "Admin.h"
 #include "Student.h"
 #include "Project.h"
+#include "ProjectControl.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
+  //just for testing stuff
+  ProjectControl pc = ProjectControl();
   char* n = "name";
-  Admin* u = new Admin(n);
+  Admin* a = new Admin(n);
+  Admin* b = new Admin(n);
   Student* s = new Student(n);
-  Project* p = new Project(n, u);
-  p->addStudent(s);
-  n = u->name;
-  std::cout << p->containsStu(s) << std::endl;
+
+  pc.createProj(a, n);
+  Project* p = pc.getProj(0);
+  pc.addStu(s, p);
+  pc.addStu(s, p);
+  std::cout << p << std::endl;
+  //pc.removeStu(s, p);
+  std::cout << s << std::endl;
+
+  std::vector<Project*> j = pc.getJoined(s);
+  std::vector<Project*> o = pc.getOwned(b);
+  std::cout << o.size() << std::endl;
   return 0;
 }
