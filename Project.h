7a28047d684efb1ae2067d9552dd74b8b48a4695
Sylthsaber <Sylthsaber@users.noreply.github.com>
2015-10-30 18:44:25 -0400

    Create Project.h

---
+++ Project.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+
+class Student;
+class Admin;
+
+class Project {
+  friend class ProjectControl;
+
+  int teamSize;
+  std::string name;
+  std::vector<Student*> students;
+  Admin* owner;
+  
+  public:
+  Project(std::string, Admin*);
+  bool containsStu(Student*);
+  Student* getStu(int);
+  void setTeamSize(int);
+  std::string getName();
+};
