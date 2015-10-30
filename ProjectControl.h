a8d201c412560143303be307b4c155920ef70585
Sylthsaber <Sylthsaber@users.noreply.github.com>
2015-10-30 18:47:29 -0400

    Create ProjectControl.h

---
+++ ProjectControl.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+
+class Project;
+class Student;
+class Admin;
+
+class ProjectControl {
+  public:
+  bool createProj(Admin*, std::string);
+  bool addStu(Student*, Project*);
+  bool removeStu(Student*, Project*);
+  Project* getProj(int);
+  void editTeamSize(Project*, int);
+
+  std::vector<Project*> getJoined(Student*);
+  std::vector<Project*> getOwned(Admin*);
+
+  private:
+  std::vector<Project*> projects;
+};
