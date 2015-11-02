e12e8a98de0fc17f4f592bf94b56d0fcd30a87d3
Sylthsaber <Sylthsaber@users.noreply.github.com>
2015-11-01 22:52:25 -0500

    Update ioFileControl.h

--- ioFileControl.h
+++ ioFileControl.h
@@ -1,8 +1,14 @@
+#ifndef IOFILECONTROL_H
+#define IOFILECONTROL_H
 #include <vector>
 #include "Student.h"
+#include "Admin.h"
 
 class ioFileControl{
 	public:
 	std::vector<Student*> stuInput(std::vector<Student*>);
+	std::vector<Admin*> admInput(std::vector<Admin*>);
 	void stuOutput(std::vector<Student*>);
+	void admOutput(std::vector<Admin*>);
 };
+#endif
