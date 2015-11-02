cbb4470684d117c9ec69a75410fa6e29103c3875
Paul <mrpaul1@hotmail.ca>
2015-11-02 14:46:08 -0500

    Update ioFileControl.h

--- ioFileControl.h
+++ ioFileControl.h
@@ -3,12 +3,16 @@
 #include <vector>
 #include "Student.h"
 #include "Admin.h"
+#include "Project.h"
+#include "ProjectControl.h"
 
 class ioFileControl{
 	public:
 	std::vector<Student*> stuInput(std::vector<Student*>);
 	std::vector<Admin*> admInput(std::vector<Admin*>);
+        void prjInput(ProjectControl*, std::vector<Admin*>, std::vector<Student*>);
 	void stuOutput(std::vector<Student*>);
 	void admOutput(std::vector<Admin*>);
+        void prjOutput(ProjectControl*);
 };
 #endif
