c949ca5819d7f09ba0d286342f7bab7750afcf41
Sylthsaber <Sylthsaber@users.noreply.github.com>
2015-11-01 22:53:30 -0500

    Update Project.h

--- Project.h
+++ Project.h
@@ -1,3 +1,5 @@
+#ifndef PROJECT_H
+#define PROJECT_H
 #include <vector>
 #include <string>
 
@@ -18,4 +20,6 @@ class Project {
   Student* getStu(int);
   void setTeamSize(int);
   std::string getName();
+  int getNumStu();
 };
+#endif
