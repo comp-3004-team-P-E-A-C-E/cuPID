e3dff74ac9dd8e5c0cd643e9a98a1d0123a14dae
Paul <mrpaul1@hotmail.ca>
2015-11-03 12:47:03 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@
 #include "Project.h"
 #include "ioFileControl.h"
 #include "ProjectControl.h"
+#include "Repository.h"
 #include <iostream>
 #include <string>
 
@@ -11,42 +12,35 @@ int main(int argc, char** argv) {
 	
 	//create the control objects
 	ProjectControl* pc = new ProjectControl();
-	ioFileControl* fc = new ioFileControl();
 	
 	//create the list of all students and admins
-	std::vector<Student*> students;
-	std::vector<Admin*> admins;
+	std::vector<Student*>* students = pc->rep->getStudents();
+	std::vector<Admin*>* admins = pc->rep->getAdmins();
 	
 	
 	//IO testing here
-	students = fc->stuInput(students);
-	admins = fc->admInput(admins);
 	std::cout <<"Everything has been read in." << std::endl;
 	
 	std::cout << "the Students: " << std::endl; 
-	for(int i =0; i<students.size();i++){
-		std::cout << "name: " << students[i]->getName() << std::endl; 
+	for(int i =0; i<students->size();i++){
+		std::cout << "name: " << (*students)[i]->getName() << std::endl; 
 	}
 	
 	std::cout << "the Admins: " << std::endl; 
-	for(int i =0; i<admins.size();i++){
-		std::cout << "name: " << admins[i]->getName() << std::endl; 
+	for(int i =0; i<admins->size();i++){
+		std::cout << "name: " << (*admins)[i]->getName() << std::endl; 
 	}
 	
-
-	std::cout << "now we output" << std::endl;
-	fc->stuOutput(students);
-	fc->admOutput(admins);
-	
 	std::cout << std::endl << "all students: " << std::endl;
-	for(int i =0; i<students.size();i++){
-		std::cout << "name: " << students[i]->getName() << std::endl;  
-		students[i]->getProfile()->printProfile();
+	for(int i =0; i<students->size();i++){
+		std::cout << "name: " << (*students)[i]->getName() << std::endl;  
+		(*students)[i]->getProfile()->printProfile();
 	}
 
         //Project Testing here
         std::cout << std::endl << "\n\n\n\n\n\n\n\n\n" << std::endl;
-        fc->prjInput(pc, admins, students);
+        ioFileControl* fc = new ioFileControl();
+        //fc->prjInput(pc, *admins, *students);
         fc->prjOutput(pc);
         Project* p = pc->getProj(0);
         std::cout << p->getName() << std::endl;
